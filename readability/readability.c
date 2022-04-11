44e2d04eae457d0f29d6012314573df173f4b3de
WorcestershireSource <alexjwilkes@gmail.com>
2022-04-11 13:06:56 +0000

    Mon, Apr 11, 2022, 1:06 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
+    // prompt user for string with get_string
+    string input = get_string("Text: ");
+    printf("%s\n", input);
+
+
+
+
+// count number of letters (a to z A2Z)
+//count number of words - anything separated by a space
+// count number of sentences - .? or! marks end of a sentence
+
+
+//calculation
+
+// print output "grade X" - as rounded int -
+//if >16 then say 16+
+//if <1 say Before Grade 1
 
 }
