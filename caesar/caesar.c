b7d13eea5cd3dc841fe8168662420ace0c0648a7
WorcestershireSource <alexjwilkes@gmail.com>
2022-04-14 06:28:41 +0000

    Thu, Apr 14, 2022, 6:28 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,41 +4,92 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+int only_digits(string);
+char rotate(char, int);
 
 int main(int argc, string argv[])
 {
-    //convert CLA string into int
-    int ciphercode = atoi(argv[1]);
-
-    //check if CLA input is valid
-    if (argc != 2 || ciphercode < 1)
+    //check that CLA is valid - just one and every character a digit
+    if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
-    //get plaintext from user
-    string plaintext = get_string("plaintext:  ");
+    if (only_digits(argv[1]) == 1)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
 
+
+    //Convert argv[1] from a string to an int
+    int cipher = atoi(argv[1]);
+
+
+    //Ask the user for the plain text input
+    string plaintext = get_string("plaintext:  ");
     int l = strlen(plaintext);
 
-    int ciphertext[l];
 
-
-    for(int i = 0; i < l; i++)
+    //print the output
+    printf("ciphertext:  ");
+    for (int i = 0; i < l; i++)
     {
-        if(islower(plaintext[i]))
-        {
-            ciphertext[i] = ((int) plaintext[0] + ciphercode) % 122;
-            printf("%ch\n", ciphercode);
-        }
-            //if(isupper(ciphertext[i]))
-            //{
-            //    ciphertext[i] = (plaintext[i] + argv[2]) % 90;
-            //}
-
-        }
-
-
+        printf("%c", rotate(plaintext[i], cipher));
+    }
+    printf("\n");
 }
 
+
+//new function to cycle through a string and check if each character is a number based on ASCII chart
+int only_digits(string s)
+{
+    int l = strlen(s);
+
+    for (int i = 0; i < l; i++)
+    {
+        if (isdigit(s[i]) == 0)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+//new function to turn a character of plaintext into a cipher code by moving it n times
+//code uses % to make sure it cycles back to start of alphabet once it has passed z
+//numbers reflect ASCII chart
+char rotate(char c, int n)
+{
+    int x = c + n;
+
+    if (isupper(c))
+    {
+        if (x > 90)
+        {
+            do
+            {
+                x = (x % 90) + 64;
+            }
+            while (x > 90);
+        }
+        return x;
+    }
+    if (islower(c))
+    {
+        if (x > 122)
+        {
+            do
+            {
+                x = (x % 122) + 96;
+            }
+            while (x > 122);
+        }
+        return x;
+    }
+    else
+    {
+        return c;
+    }
+}
