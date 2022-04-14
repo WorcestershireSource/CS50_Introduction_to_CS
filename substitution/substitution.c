e20f272bfcc0838f13a2ff416457371c10ac47f6
WorcestershireSource <alexjwilkes@gmail.com>
2022-04-14 07:35:28 +0000

    Thu, Apr 14, 2022, 7:35 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -15,7 +15,7 @@ int main(int argc, string argv[])
         printf("Usage: ./substitution key\n");
         return 1;
     }
-    if (strlen(argv) != 26)
+    if (strlen(argv[1]) != 26)
     {
         printf("Key must contain 26 characters.\n");
         return 1;
