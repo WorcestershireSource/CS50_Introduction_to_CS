926ed6d2e36dcf4930b5444be5215bb92e08aeab
WorcestershireSource <alexjwilkes@gmail.com>
2022-04-21 04:55:57 +0000

    Thu, Apr 21, 2022, 4:55 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -179,7 +179,7 @@ int find_min(void)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if (canditates[i].eliminated == false && candidates[i].votes < lowest_score)
+        if (candidates[i].eliminated == false && candidates[i].votes < lowest_score)
         {
             lowest_score = candidates[i].votes;
         }
