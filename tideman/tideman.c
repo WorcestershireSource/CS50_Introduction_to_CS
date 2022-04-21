8146a8af49489c721ffec7877b883c3f242c0032
WorcestershireSource <alexjwilkes@gmail.com>
2022-04-21 14:03:16 +0000

    Thu, Apr 21, 2022, 2:03 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -139,14 +139,14 @@ void add_pairs(void)
         {
             if (preferences[i][j + 1] > preferences[j + 1][i])
             {
-                pairs[pcount].winner = preferences[i][j + 1];
-                pairs[pcount].loser = preferences[j + 1][i];
+                pairs[pcount].winner = i;
+                pairs[pcount].loser = j+1;
                 pcount++;
             }
             if (preferences[i][j + 1] < preferences[j + 1][i])
             {
-                pairs[pcount].loser = preferences[i][j + 1];
-                pairs[pcount].winner = preferences[j + 1][i];
+                pairs[pcount].loser = j + 1;
+                pairs[pcount].winner = i;
                 pcount++;
             }
         }
