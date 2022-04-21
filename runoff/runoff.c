7182b4354e4c6031d425fd9cd2471ac510d8134a
WorcestershireSource <alexjwilkes@gmail.com>
2022-04-21 04:00:49 +0000

    Thu, Apr 21, 2022, 4:00 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,10 @@ bool vote(int voter, int rank, string name)
     //check if name is match for valid candidate
     for(int i = 0; i < candidate_count; i++)
     {
-        if(strcmp(name, ))
+        if(strcmp(name, candidates[i].name) == 0)
+        {
+            candidates[].votes
+        }
     }
     //update preferences array
     //return true/false
