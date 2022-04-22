dc488792d3b08ffdc77a9d07df3b9a9f9cdb2388
WorcestershireSource <alexjwilkes@gmail.com>
2022-04-22 07:55:29 +0000

    Fri, Apr 22, 2022, 7:55 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -201,9 +201,9 @@ void print_winner(void)
     {
         int cancelled = 0;
 
-        for (int j = 0; j < pairs_count; i++)
+        for (int j = 0; j < candidate_count; i++)
         {
-            if (locked[pairs[j].winner][i] == true)
+            if (locked[j][i] == true)
             {
                 cancelled++;
             }
