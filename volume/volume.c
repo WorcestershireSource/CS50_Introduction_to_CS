37056986903367031ba5bf491460b46980d05598
WorcestershireSource <alexjwilkes@gmail.com>
2022-04-26 09:26:07 +0000

    Tue, Apr 26, 2022, 9:26 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -36,9 +36,8 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
 
-    fread(&header, 8, HEADER_SIZE, input);
-
-    fwrite(&header, 8, HEADER_SIZE, output);
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(&header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
