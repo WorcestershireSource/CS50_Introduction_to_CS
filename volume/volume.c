4ef7414b829dadd7608d138f3c92722d2760377c
WorcestershireSource <alexjwilkes@gmail.com>
2022-04-26 09:16:58 +0000

    Tue, Apr 26, 2022, 9:16 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
     fwrite(header, 8, HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
-    int16_t buffer;
+    int16_t buffer[];
 
     //do I need dynamic memory allocation for buffer?
 
