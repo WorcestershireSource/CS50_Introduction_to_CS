1c0ccf9d175117284324a5909d2b4dfe23ebe3da
WorcestershireSource <alexjwilkes@gmail.com>
2022-04-26 08:37:52 +0000

    Tue, Apr 26, 2022, 8:37 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
 
-    
+    size_t fwrite(void *READ, size_t 44, size_t 44, FILE *)
 
     // TODO: Read samples from input file and write updated data to output file
 
