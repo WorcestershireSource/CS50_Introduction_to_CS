8299b16a7423c789c01dc2196d426b51141b0abc
WorcestershireSource <alexjwilkes@gmail.com>
2022-04-27 08:58:54 +0000

    Wed, Apr 27, 2022, 8:58 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -10,5 +10,22 @@ if (argc != 2)
     return 1;
 }
 
+FILE *input = fopen(argv[1], "r");
+if (input == NULL)
+{
+    printf("Could not open file.\n");
+    return 1;
+}
+
+FILE *output = fopen(argv[2], "w");
+if (output == NULL)
+{
+    printf("Could not open file.\n");
+    return 1;
+}
+
+uint8_t copy[512];
+
+fread(copy, 512, 1, input);
 
 }
