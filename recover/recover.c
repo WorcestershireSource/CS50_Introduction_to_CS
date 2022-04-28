9db673f1212f4f71108d60cdeda0f70d5f791e87
WorcestershireSource <alexjwilkes@gmail.com>
2022-04-28 01:30:55 +0000

    Thu, Apr 28, 2022, 1:30 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -24,13 +24,19 @@ if (output == NULL)
     return 1;
 }
 
-//check for 0xff 0xd8 0xff at first byte followed by byte beginning with 0xe? (1110)
-uint8_t copy[512];
+//check for 0xff 0xd8 0xff at first three bytes followed by byte beginning with 0xe? (1110)
+
+// check three bytes, check first half of fourth
+// take 512 bytes, including four checked (4 + 508)
+// if JPEG, write to new output file
+// repeat
+
+int8_t copy[512];
 
 if(fread(copy, sizeof(uint8_t), 1, input))
 
 {
-    
+
 }
 
 }
