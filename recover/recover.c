56ccd9fd87f856fa578f9490bef9063915e9c506
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-02 07:41:55 +0000

    Mon, May 2, 2022, 7:41 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -57,7 +57,7 @@ while(fread(&copy, 1, 512, input) == 512)
 //JPEG stored back to back - keep saving 512 blocks until find one that starts with header file
 //video shows way to handle fourth byte
 
-//
+//sprintf 7 mins
 
 //Open a new file with unique name - dynamic memory allocation
 
