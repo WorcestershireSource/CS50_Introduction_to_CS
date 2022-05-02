96da63af19bb0c030dbb4b1df618725e02a5f59a
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-02 08:25:26 +0000

    Mon, May 2, 2022, 8:25 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,53 +5,55 @@
 int main(int argc, char *argv[])
 {
 
-if (argc != 2)
-{
-    printf("Usage ./recover filename\n");
-    return 1;
-}
-
-//open memory card - fopen r
-FILE *input = fopen(argv[1], "r");
-if (input == NULL)
-{
-    printf("Could not open file.\n");
-    return 1;
-}
-
-
-typedef uint8_t BYTE;
-
-BYTE buffer[512];
-
-int JPEG_count = 0;
-
-//loop until fread = 0 (EOF) & read 512 bytes into a buffer
-while(fread(buffer, sizeof(BYTE), 512, input) != 0)
-{
-    //if start of new JPEG - JPEG header {0xff, 0xd8, 0xff} - plus four bits
-    if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+    if (argc != 2)
     {
-        JPEG_count++;
-        //if first JPEG - open new file and write else close previous and open new file
-        if(JPEG_count == 1)
-        {
-            char name[4];
-            sprintf(name, "%03i.jpg",JPEG_count);
+        printf("Usage ./recover filename\n");
+        return 1;
+    }
 
-            FILE* img = fopen(name, "w");
-            if(img == NULL)
+    //open memory card - fopen r
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+
+    typedef uint8_t BYTE;
+
+    BYTE buffer[512];
+
+    int JPEG_count = 0;
+
+    //loop until fread = 0 (EOF) & read 512 bytes into a buffer
+    while(fread(buffer, sizeof(BYTE), 512, input) != 0)
+    {
+        //if start of new JPEG - JPEG header {0xff, 0xd8, 0xff} - plus four bits
+        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            JPEG_count++;
+            //if first JPEG - open new file and write else close previous and open new file
+            if(JPEG_count == 1)
             {
-                printf("Could not open new file\n");
-                return 1;
-            }
+                char name[4];
+                sprintf(name, "%03i.jpg",JPEG_count);
 
-            fwrite(buffer, sizeof(BYTE), 512, img);
+                FILE* img = fopen(name, "w");
+                if(img == NULL)
+                {
+                    printf("Could not open new file\n");
+                    return 1;
+                }
+
+                fwrite(buffer, sizeof(BYTE), 512, img);
+            }
         }
-        if(JPEG_count > 1)
-        {
-            
-        }
+        else
+            if(JPEG_count > 1)
+            {
+                fwrite(buffer, sizeof(BYTE), 512, img);
+            }
 
     }
 }
