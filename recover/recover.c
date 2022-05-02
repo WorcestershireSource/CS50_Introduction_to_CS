424d95f2da93d2f04e99a18737eab97cfcd35c5f
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-02 08:37:12 +0000

    Mon, May 2, 2022, 8:37 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -26,6 +26,8 @@ int main(int argc, char *argv[])
 
     int JPEG_count = 0;
 
+    int* ptr = NULL;
+
     //loop until fread = 0 (EOF) & read 512 bytes into a buffer
     while(fread(buffer, sizeof(BYTE), 512, input) != 0)
     {
@@ -40,6 +42,7 @@ int main(int argc, char *argv[])
                 sprintf(name, "%03i.jpg",JPEG_count);
 
                 FILE* img = fopen(name, "w");
+                ptr = img;
                 if(img == NULL)
                 {
                     printf("Could not open new file\n");
@@ -47,15 +50,12 @@ int main(int argc, char *argv[])
                 }
 
                 fwrite(buffer, sizeof(BYTE), 512, img);
-
-                fclose(img);
             }
         }
         else
             if(JPEG_count > 1)
             {
-                fwrite()
-                fwrite(buffer, sizeof(BYTE), 512, &img);
+                fwrite(buffer, sizeof(BYTE), 512, *ptr)
             }
 
     }
