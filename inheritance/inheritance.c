35f22834cd7e1a249232e506bdbd600473926ae5
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-04 06:29:49 +0000

    Wed, May 4, 2022, 6:29 AM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -53,20 +53,24 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        new_person->parent[0] = parent0;
-        new_person->parent[1] = parent1;
+        new_person->parents[0] = parent0;
+        new_person->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-    
+        new_person->alleles[0] = parent[0].alleles[round((float) random() / ((double() RAND_MAX + 1)))];
+        new_person->alleles[1] = ;
     }
 
     // If there are no generations left to create
     else
     {
         // TODO: Set parent pointers to NULL
+        new_person->parents[0] = NULL;
+        new_person->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-
+        new_person->alleles[0] = randomallele();
+        new_person->alleles[0] = randomallele();
     }
 
     // TODO: Return newly created person
