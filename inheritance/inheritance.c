720276d8f5e072fb727e9950f0e153d24a784378
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-04 06:49:13 +0000

    Wed, May 4, 2022, 6:49 AM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -57,8 +57,8 @@ person *create_family(int generations)
         new_person->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        new_person->alleles[0] = parent0->alleles[rand() % 2];
-        new_person->alleles[1] = parent1->alleles[rand() % 2];
+        new_person->alleles[0] = parent0->alleles[0];
+        new_person->alleles[1] = parent1->alleles[1];
     }
 
     // If there are no generations left to create
