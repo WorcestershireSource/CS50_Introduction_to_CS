5ac178bf0c067a11cfa9eea00319acc0c5ebfe76
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-09 04:36:29 +0000

    Mon, May 9, 2022, 4:36 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -97,8 +97,7 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    if ()
-    return 0;
+    return dict_size;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
