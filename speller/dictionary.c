53c31b2ed6fbb00b77dd4f883938fe834fea51a7
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-09 04:32:09 +0000

    Mon, May 9, 2022, 4:32 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -107,7 +107,12 @@ bool unload(void)
     for (int i = 0; i < N; i++)
     {
         node *cursor = table[i];
-        
+        node *tmp = cursor;
+        while (cursor->next != NULL)
+        {
+            cursor = cursor->next;
+            free(tmp);
+        }
     }
     return false;
 }
