2286911064b4fdaca0fb2d0d77814abc6ac2b32b
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-10 10:55:50 +0000

    Tue, May 10, 2022, 10:55 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -16,7 +16,7 @@ elif two_digit == 34 or two_digit == 37:
 luhn = 0
 
 for i in range(17):
-    if i % 2 == 0 and (((card % 10) *2) > 9):
+    if i % 2 == 0 and (((card % 10) * 2) > 9):
         luhn += (((card % 10) * 2) % 10) + ((((card % 10) * 2) / 10) % 10)
     elif (i % 2 > 0):
         luhn += (card % 10)
