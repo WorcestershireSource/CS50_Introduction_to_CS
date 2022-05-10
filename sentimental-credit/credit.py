862ac1d73b579f8ae7901089c4915926633131a4
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-10 12:10:41 +0000

    Tue, May 10, 2022, 12:10 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -7,11 +7,11 @@ c_length = len(str_card)
 
 two_digit = int(card / (10 ** (c_length - 2)))
 
-if two_digit < 50 or two_digit > 39 and (c_length == 13 or c_length == 16):
+if (two_digit < 50 or two_digit > 39) and (c_length == 13 or c_length == 16):
     company = "VISA"
-elif two_digit < 56 and two_digit > 50 and c_length == 16:
+elif (two_digit < 56 and two_digit > 50) and c_length == 16:
     company = "MASTERCARD"
-elif two_digit == 34 or two_digit == 37 and c_length == 15:
+elif (two_digit == 34 or two_digit == 37) and c_length == 15:
     company = "AMEX"
 
 
