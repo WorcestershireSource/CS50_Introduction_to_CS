f037ea038efc1b46875f0f51344a07cb5904bb3a
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-10 10:21:28 +0000

    Tue, May 10, 2022, 10:21 AM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,13 +1,15 @@
 from cs50 import get_int
 
-height = get_int("Height: ")
+while True:
+    height = get_int("Height: ")
+    if height < 9 and height > 0:
+        break
 
-if height < 9 and height > 0:
-    for i in range(height + 1):
-        print(" " * (height - i), end="")
-        print("#" * i, end="")
-        print("  ", end="")
-        print("#" * i)
+for i in range(height + 1):
+    print(" " * (height - i), end="")
+    print("#" * i, end="")
+    print("  ", end="")
+    print("#" * i)
 
 
 
