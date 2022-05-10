a5f3e06ecdbc14570d5267aded07690a71c0e078
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-10 12:07:08 +0000

    Tue, May 10, 2022, 12:07 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -17,18 +17,19 @@ elif two_digit == 34 or two_digit == 37 and c_length == 15:
 
 luhn = 0
 
-for i in range(0, c_length, 2):
+for i in range(0, c_length + 1, 2):
     luhn += int(str_card[c_length - (i + 1)])
     print(str(i) + " digit luhn is " + str(luhn))
-    z = int(str_card[c_length - (i + 2)]) * 2
-    print(str(z))
-    if z > 9:
-        luhn += (z - 10) + 1
-    elif z < 10:
-        luhn += z
-    print(str(i + 1) + " digit luhn is " + str(luhn))
+    if c_length - (i + 2) > -1:
+        z = int(str_card[c_length - (i + 2)]) * 2
+        print(str(z))
+        if z > 9:
+            luhn += (z - 10) + 1
+        elif z < 10:
+            luhn += z
+        print(str(i + 1) + " digit luhn is " + str(luhn))
 
-print(str_card[1])
+print(str_card[0])
 
 if luhn % 10 == 0:
     print(company)
