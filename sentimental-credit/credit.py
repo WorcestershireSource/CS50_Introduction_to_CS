b256f770242e0204e5bff4bea60f00744ddc3da8
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-10 11:26:17 +0000

    Tue, May 10, 2022, 11:26 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -5,7 +5,7 @@ str_card = str(card)
 
 c_length = len(str_card)
 
-two_digit = card / (10 ** (c_length - 2))
+two_digit = int(card / (10 ** (c_length - 2)))
 
 if two_digit < 50 or two_digit > 39:
     company = "VISA"
@@ -14,21 +14,25 @@ elif two_digit < 56 and two_digit > 50:
 elif two_digit == 34 or two_digit == 37:
     company = "AMEX"
 
+print(two_digit)
+
 luhn = 0
 
 for i in range(0, c_length, 2):
     luhn += int(str_card[c_length - (i + 1)])
-
+    print(str(i) + " digit luhn is " + str(luhn))
     z = int(str_card[c_length - (i + 2)]) * 2
     if z > 9:
         luhn += (z - 10) + 1
     elif z < 10:
         luhn += z
+        print(str(i + 1) + " digit luhn is " + str(luhn))
 
 if luhn % 10 == 0:
     print(company)
 else:
     print("INVALID")
+
 
 
 
