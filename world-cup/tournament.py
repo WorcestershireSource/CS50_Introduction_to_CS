c69bae81acaf77bdb1e0876427cedc83a5b9012f
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-11 03:37:45 +0000

    Wed, May 11, 2022, 3:37 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,6 +16,9 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
+    file = open(sys.argv[1], "r")
+    csv.DictReader(file)
+    
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
