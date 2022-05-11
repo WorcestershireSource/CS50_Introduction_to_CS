21f771529a1d2e6a55b06b4395fae626415e32e9
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-11 00:57:29 +0000

    Wed, May 11, 2022, 12:57 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,10 +2,18 @@ from cs50 import get_string
 
 input = get_string("Text: ")
 
-# Count words
+# Count words sentences and letters in string
 word_list = input.split()
-number_words = len(word_list)
+num_words = len(word_list)
+num_letters = sum(c.isalpha() for c in input)
+num_sentences = input.count('.' or '?' or '!')
 
-letters = sum(c.isalpha() for c in input)
+# Calculate averages and readability index score
+av_letters = (num_letters / num_words) * 100
+av_words = (num_sentences / num_words) * 100
+index = round((0.0588 * av_letters) - (0.296 * av_words) - 15.8)
 
-sentences = filtered.count('.' or '!' or '?')
+if index < 1:
+    print("Before Grade 1")
+elif index > 1 and 
+
