e73aee2155f66bb68a1d129dff7d825f5e30700f
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-11 03:54:40 +0000

    Wed, May 11, 2022, 3:54 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -21,7 +21,8 @@ def main():
         for row in reader:
             team = row["team"]
             rating = row["rating"]
-            teams.append(team, rating)
+            teams.append(team)
+            teams.append(rating)
 
 
     counts = {}
