9c039c5eff54487e0b81382ef2084d265bb25c98
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-11 00:22:26 +0000

    Wed, May 11, 2022, 12:22 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -21,22 +21,20 @@ luhn = 0
 
 for i in range(1, c_length, 2):
     luhn += int(str_card[c_length - (i)])
-    print(str(i + 1) + " digit " + str(luhn))
+    # print(str(i + 1) + " digit " + str(luhn))
     if c_length - (i + 1) > -1:
         z = int(str_card[c_length - (i + 1)]) * 2
         if z > 9:
             luhn += (z - 10) + 1
         elif z < 10:
             luhn += z
-        print(str(i + 2) + " digit 2 " + str(luhn))
+        # print(str(i + 2) + " digit 2 " + str(luhn))
 
 
 if luhn % 10 == 0:
     print(company)
 else:
     print("INVALID")
-
-print(str(c_length))
 
 
 
