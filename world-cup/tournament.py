94caca68222e1e6b90a4a4c675fb4063248cbc45
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-11 04:39:02 +0000

    Wed, May 11, 2022, 4:39 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -21,7 +21,7 @@ def main():
         reader = csv.DictReader(file)
         for row in reader:
             team = row["team"]
-            rating = row["rating"]
+            rating = int(row["rating"])
             teams.append(team)
             teams.append(rating)
 
