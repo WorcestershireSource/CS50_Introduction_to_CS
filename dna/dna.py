2cd91adaed1362545f8e5f6bc4ee2f9b08572bd8
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-12 04:39:16 +0000

    Thu, May 12, 2022, 4:39 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -39,7 +39,7 @@ def main():
     count = 0
     for i in range(num):
         for j in range(x):
-            print(dna[j]["count"] + " " + people[i][dna[j]["dna"] + " end ")
+            # print(dna[j]["count"] + " " + people[i][dna[j]["dna"]] + " end ")
             if dna[j]["count"] == people[i][dna[j]["dna"]]:
                 count += 1
 
