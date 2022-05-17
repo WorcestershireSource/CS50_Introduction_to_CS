dfca91f4b1028370504b3a8b53505c855f2c1551
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-17 05:50:55 +0000

    Tue, May 17, 2022, 5:50 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -5,7 +5,7 @@ SELECT DISTINCT(title) FROM movies
         JOIN stars ON movies.id = stars.movie_id
         JOIN people ON stars.person_id = people.id
         WHERE name = "Helena Bonham Carter")
-        AND title IN (SELECT title FROM movies
-        JOIN stars ON movies.id = stars.movie_id
-        JOIN people ON stars.person_id = people.id
-        WHERE name = "Johnny Depp");
+            AND title IN (SELECT title FROM movies
+            JOIN stars ON movies.id = stars.movie_id
+            JOIN people ON stars.person_id = people.id
+            WHERE name = "Johnny Depp");
