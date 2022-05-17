6137c44e96660ba8a630adb1de19aa637d086a2b
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-17 03:30:51 +0000

    Tue, May 17, 2022, 3:30 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,6 +1,6 @@
 SELECT name FROM people
     JOIN stars ON people.id = stars.person_id
-    WHERE movie_id  (SELECT movie_id WHERE (name = "Kevin Bacon" AND birth = 1958));
+    WHERE movie_id = (SELECT movie_id WHERE (name = "Kevin Bacon" AND birth = 1958));
 
 SELECT title, movie_id FROM movies
     JOIN stars ON movies.id = stars.movie_id
