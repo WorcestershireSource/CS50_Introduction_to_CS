a1732b2e5d2f293cff98180807a64a8557575bbb
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-17 04:11:40 +0000

    Tue, May 17, 2022, 4:11 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,4 @@
 SELECT title FROM movies
     JOIN stars ON movies.id = stars.movie_id
     JOIN people ON stars.person_id = people.id
-    WHERE name = "Johnny Depp" OR "Helena Bonham Carter";
+    WHERE name = "Johnny Depp" AND "Helena Bonham Carter";
--- movies/13.sql
+++ movies/13.sql
@@ -4,12 +4,3 @@ SELECT name FROM people
         JOIN people on stars.person_id = people.id
         WHERE (name = "Kevin Bacon" AND birth = 1958))
     AND name != "Kevin Bacon";
-
-SELECT title, movie_id FROM movies
-    JOIN stars ON movies.id = stars.movie_id
-    JOIN people ON stars.person_id = people.id
-    WHERE (name = "Kevin Bacon" AND birth = 1958);
-
-SELECT name FROM people
-    JOIN stars ON people.id = stars.person_id
-    WHERE movie_id IN (112384, 112453);
