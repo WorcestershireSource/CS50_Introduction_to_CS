358c1a600f96c8d85aad9adbba9ef2985070bc8f
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-17 06:03:32 +0000

    Tue, May 17, 2022, 6:03 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-
+-- Check the crime scene logs - shows time of 10:15 and says 3x transcripts mention bakery
 SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
+
+SELECT activity, license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute = 15;
