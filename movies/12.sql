8bdb72ebda4aa2aeaa3ec1b9bbb48f5da7446fdb
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-17 05:35:44 +0000

    Tue, May 17, 2022, 5:35 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,8 +1,8 @@
-SELECT title FROM movies
+SELECT title FROM (SELECT title FROM movies
     JOIN stars ON movies.id = stars.movie_id
     JOIN people ON stars.person_id = people.id
-    WHERE (title IN (SELECT title FROM movies WHERE name = "Johnny Depp")
-    AND title IN (SELECT title FROM movies WHERE name = "Helena Bonham Carter"));
+    WHERE title IN (SELECT title FROM movies WHERE name = "Johnny Depp")))
+    WHERE title IN (SELECT title FROM movies WHERE name = "Helena Bonham Carter");
 
 SELECT title FROM movies
     WHERE id IN (SELECT movie_id FROM stars
