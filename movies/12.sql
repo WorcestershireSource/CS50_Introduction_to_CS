3037307686492550a127c09b254c5f4ee19e2cbb
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-17 05:22:31 +0000

    Tue, May 17, 2022, 5:22 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,4 @@
-SELECT title FROM movies
+SELECT title FROM movies, stars, people
     IN (SELECT title FROM movies
         JOIN stars ON movies.id = stars.movie_id
         JOIN people ON stars.person_id = people.id
