0949da5fae31f12bc43d7f0f5a04c929b1c7397a
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-17 04:17:59 +0000

    Tue, May 17, 2022, 4:17 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,4 @@
 SELECT title FROM movies
     JOIN stars ON movies.id = stars.movie_id
     JOIN people ON stars.person_id = people.id
-    WHERE name = "Johnny Depp" AND name = "Helena Bonham Carter";
+    WHERE "Johnny Depp" AND name = "Helena Bonham Carter";
