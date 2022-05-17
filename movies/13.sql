c8b4747be9a2c6305535adb570d9d83fff873991
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-17 03:01:36 +0000

    Tue, May 17, 2022, 3:01 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,4 @@
 SELECT names FROM people
     JOIN stars ON people.id = stars.person_id
     JOIN movies ON stars.movie_id = movies.id
-    WHERE name = "Johnny Depp" OR "Helena Bonham Carter";
+    WHERE (name = "Kevin Bacon" AND birth = 1958);
