590f912fb4e80db4e91a8ee7a0c5bff461c99de2
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-17 05:46:01 +0000

    Tue, May 17, 2022, 5:46 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -3,6 +3,17 @@ SELECT title FROM movies
     JOIN people ON stars.person_id = people.id
     WHERE name = "Helena Bonham Carter";
 
+
+
+SELECT name FROM people
+    JOIN stars ON people.id = stars.person_id
+    WHERE movie_id IN (SELECT movie_id FROM stars
+        JOIN people on stars.person_id = people.id
+        WHERE (name = "Kevin Bacon" AND birth = 1958))
+    AND name != "Kevin Bacon";
+
+    
+
 SELECT title FROM movies
     WHERE id IN (SELECT movie_id FROM stars
         WHERE person_id )
