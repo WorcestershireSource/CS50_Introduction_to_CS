9acf5ec9963ff69572665ebb1f77eb15fa253f92
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-17 05:49:55 +0000

    Tue, May 17, 2022, 5:49 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,33 +1,11 @@
-SELECT title FROM movies
+SELECT DISTINCT(title) FROM movies
     JOIN stars ON movies.id = stars.movie_id
     JOIN people ON stars.person_id = people.id
-    WHERE name = "Helena Bonham Carter";
-
-
-
-SELECT name FROM people
-    JOIN stars ON people.id = stars.person_id
-    WHERE movie_id IN (SELECT movie_id FROM stars
-        JOIN people on stars.person_id = people.id
-        WHERE (name = "Kevin Bacon" AND birth = 1958))
-    AND name != "Kevin Bacon";
-
-    
-
-SELECT title FROM movies
-    WHERE id IN (SELECT movie_id FROM stars
-        WHERE person_id )
-
-
-SELECT title FROM movies
-    WHERE id IN (SELECT movie_id FROM stars
-        WHERE person_id IN (SELECT id FROM people
-            WHERE name LIKE 'Helena Bonham Carter') AND (SELECT id FROM people
-                WHERE name LIKE 'Johnny Depp'));
-
-
-JD = 136
-
-HBC = 307
-
-Corpse Bride  = 121164
+    WHERE title IN (SELECT title FROM movies
+        JOIN stars ON movies.id = stars.movie_id
+        JOIN people ON stars.person_id = people.id
+        WHERE name = "Helena Bonham Carter")
+        AND title IN (SELECT title FROM movies
+        JOIN stars ON movies.id = stars.movie_id
+        JOIN people ON stars.person_id = people.id
+        WHERE name = "Johnny Depp");
