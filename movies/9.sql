4b4d11a1cec0721ba2d7fb96bf83c816a620a0a4
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-17 02:48:16 +0000

    Tue, May 17, 2022, 2:48 AM +00:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,4 +1,4 @@
-SELECT name FROM people
+SELECT DISTINCT(name) FROM people
     JOIN stars ON people.id = stars.person_id
     JOIN movies ON stars.movie_id = movies.id
-    WHERE year = 2004 ORDER BY birth
+    WHERE year = 2004 ORDER BY birth;
