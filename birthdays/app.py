dbd6837bc72e6addec1ba61358bb8ffcf1935231
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-26 09:26:46 +0000

    Thu, May 26, 2022, 9:26 AM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -30,7 +30,7 @@ def index():
         day = request.form.get("day")
         month = request.form.get("month")
 
-        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, day, month)
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
 
         return redirect("/")
 
