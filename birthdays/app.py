fa53f46afca620a549bf0d23996197812b5282ca
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-26 09:32:54 +0000

    Thu, May 26, 2022, 9:32 AM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -39,5 +39,6 @@ def index():
         # Display the entries in the database on index.html
         birthdays = db.execute("SELECT * FROM birthdays")
         return render_template("index.html", birthdays=birthdays)
+    
 
 
