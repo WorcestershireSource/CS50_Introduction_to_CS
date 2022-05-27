f98c9620b10ea3e126048ebeab00d3b1ec9a1c4e
WorcestershireSource <alexjwilkes@gmail.com>
2022-05-27 04:28:13 +0000

    Fri, May 27, 2022, 4:28 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -116,7 +116,13 @@ def quote():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user"""
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
     return apology("TODO")
 
 
